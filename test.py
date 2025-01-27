10ad215857ee1dfbee0425d8b769c69eebe0a0cc
xik <xikest12@gmail..com>
2025-01-27 22:10:44 +0900

    .

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ url = "https://app-ydown-646543566973.asia-northeast3.run.app/download/"
 # url = "https://127.0.0.0:8080/download/"
 data = {
     "url": "https://youtu.be/SlPhMPnQ58k?si=55RxR7mWvOWtMwGC",  # 실제 유튜브 URL로 대체
-    "file_type": "mp3"  # 또는 "mp4"
+    "file_type": "mp4"  # 또는 "mp4"
 }
 
 # POST 요청 보내기
