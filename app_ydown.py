43b5f737640b6180dd304974644e60bdaaaa6ee4
xik <xikest12@gmail..com>
2025-01-28 00:40:15 +0900

    .

--- app_ydown.py
+++ app_ydown.py
@@ -10,7 +10,7 @@ app = FastAPI()
 ydt = YTDownloader()
 bucket_name = "web-ydown-storage"
 storage_manager = StorageManager("web-driver.json")
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.ERROR)
 
 
 class DownloadRequest(BaseModel):
@@ -28,25 +28,26 @@ async def download_file(request: DownloadRequest):
     public_url = storage_manager.get_public_url_if_file_exists(bucket_name = bucket_name, file_name=filename)
     
     if public_url is None:
-        file_path = ydt.download_video(request.url, request.file_type)
-        logging.info(f"Downloaded file path: {file_path}") 
+        file_path = ydt.download_video(video_url=request.url, file_type=request.file_type, filename_replaced=filename)
+        logging.info(f"Downloaded file: {file_path}") 
         
-        blob_name = file_path.split("/")[-1]
+        blob_name = file_path
         storage_manager.upload_file(bucket_name, file_path, blob_name)
         blob = storage_manager.client.bucket(bucket_name).blob(blob_name)
         blob.make_public()
         public_url = blob.public_url
         if not os.path.exists(file_path):
             raise HTTPException(status_code=500, detail="Error downloading the file.")
-        
+        message= "new"
         logging.info(f"new URL: {public_url}")
     else:
+        message= "old"
         logging.info(f"exist URL: {public_url}")
     
     return JSONResponse(
         status_code=200,
         content={
-            "message": "success",
+            "message": message,
             "file_name": public_url
         }
     )
--- functions/yt_downloader.py
+++ functions/yt_downloader.py
@@ -1,9 +1,13 @@
 import yt_dlp
 import logging
+import os 
 
 class YTDownloader:
-    @staticmethod
-    def get_video_filename(url: str, file_type: str) -> str:
+    
+    def __init__(self):
+        pass 
+    
+    def get_video_filename(self, url: str, file_type: str) -> str:
         ydl_opts = {
             'quiet': True,  # 출력 최소화
             'format': 'bestaudio/best',  # 가장 좋은 품질 선택
@@ -12,10 +16,11 @@ class YTDownloader:
             # 동영상 메타데이터 가져오기
             info_dict = ydl.extract_info(url, download=False)  # 다운로드하지 않음
             title = info_dict.get('title', 'unknown_title')  # 동영상 제목
+            title= title.strip()
             return f"{title}.{file_type}"  # 파일 이름 생성    
     
-    @staticmethod
-    def download_video(video_url: str, file_type: str) -> str:
+    
+    def download_video(self, video_url: str, file_type: str, filename_replaced:str=None) -> str:
         options =  {
                     "mp3": {
                         "format": "bestaudio/best",
@@ -52,9 +57,10 @@ class YTDownloader:
             with yt_dlp.YoutubeDL(options.get(file_type)) as ydl:
                 result = ydl.extract_info(video_url, download=True)
                 file_name = ydl.prepare_filename(result)
-                
-                # .webm 파일을 mp4 또는 mp3로 변환하지 않고 그대로 저장
                 file_name = file_name.replace(".webm", f".{file_type}")
+                if filename_replaced is not None:
+                    os.rename(file_name, filename_replaced)
+                    file_name = filename_replaced
                 return file_name
 
         except yt_dlp.utils.DownloadError as e:
