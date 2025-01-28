d2a1a8a16cadc63ba2538abf68540563b02c6e12
xik <xikest12@gmail..com>
2025-01-28 14:23:59 +0900

    .

--- app_ydown.py
+++ app_ydown.py
@@ -27,40 +27,41 @@ async def download_file(request: DownloadRequest):
     if request.file_type not in ["mp3", "mp4"]:
         raise HTTPException(status_code=400, detail="Invalid file type. Only 'mp3' or 'mp4' allowed.")
     filename = ydt.get_video_filename(url=request.url, file_type=request.file_type)
-    public_url = storage_manager.get_public_url_if_file_exists(bucket_name = bucket_name, file_name=filename)
+    signed_url = storage_manager.get_signed_url_if_file_exists(bucket_name = bucket_name, file_name=filename)
     
-    if public_url is None:
+    if signed_url is None:
         try:
-            file_path = ydt.download_video(video_url=request.url, file_type=request.file_type, filename_replaced=filename)
-            logging.info(f"Downloaded file: {file_path}") 
+            # file_name : xx.mp
+            file_name = ydt.download_video(video_url=request.url, file_type=request.file_type, filename_replaced=filename)
+            logging.info(f"Downloaded file: {file_name}") 
             
-            blob_name = file_path
-            storage_manager.upload_file(bucket_name, file_path, blob_name)
+            file_path = file_name
+            storage_manager.upload_file(bucket_name, file_path, file_name)
             
             if os.path.exists(file_path):
                 os.unlink(file_path)
             else:
-                logging.warning(f"File {file_path} not found for deletion.")
+                logging.warning(f"File {file_name} not found for deletion.")
             
-            blob = storage_manager.client.bucket(bucket_name).blob(blob_name)
-            blob.make_public()
-            public_url = blob.public_url
+            
+            signed_url = storage_manager.get_signed_url_if_file_exists(bucket_name = bucket_name, file_name=file_name)
+
             
             message= "new"
-            logging.info(f"new URL: {public_url}")
+            logging.info(f"new URL: {signed_url}")
             
         except Exception as e:
             logging.error(f"Error processing file: {str(e)}")
             raise HTTPException(status_code=500, detail=f"Error processing file: {str(e)}")
     else:
         message= "old"
-        logging.info(f"exist URL: {public_url}")
+        logging.info(f"exist URL: {signed_url}")
     
     return JSONResponse(
         status_code=200,
         content={
             "message": message,
-            "file_name": public_url
+            "file_name": signed_url
         }
     )
     
